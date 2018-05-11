df3cb11dd6271f30885cbb56bee2b2c5b9746de8
Ashish Patel <patel.ashish610@gmail.com>
2018-05-11 17:12:07 +0530

    Initial Project Commit

---
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.devopsbuddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "index";
+    }
+
+}
